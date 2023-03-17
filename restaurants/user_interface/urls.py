dce81b0f8d5c044ee2d5c1594dd84e8cbe162cf8
jiezzzzzzz <jiezzzzzzzzz@mail.ru>
2023-03-17 12:41:26 +0300

    fixed bugs

--- restaurants/user_interface/urls.py
+++ restaurants/user_interface/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import start_page
+from .views import start_page, home
 
 
 urlpatterns = [
     path('', start_page),
+    path('home/', home)
 ]
--- restaurants/user_interface/views.py
+++ restaurants/user_interface/views.py
@@ -1,5 +1,25 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Админ',
+        'title': 'Это первый пост',
+        'content': 'Содержание первого поста.',
+        'date_posted': '12 мая, 2022'
+    },
+    {
+        'author': 'Пользователь',
+        'title': 'Это второй пост',
+        'content': 'Подробное содержание второго поста.',
+        'date_posted': '13 мая, 2022'
+    }
+]
 
+
+def home(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'user_interface/home.html', context)
 def start_page(request):
-    return render(request, 'home.html')
+    return render(request, 'user_interface/about.html')
