91f35dd84faf4d4cdf9108d1c3f4b64a92d9fa41
jiezzzzzzz <jiezzzzzzzzz@mail.ru>
2023-04-02 20:55:21 +0300

    fixed bugs

--- restaurants/booking_request/urls.py
+++ restaurants/booking_request/urls.py
@@ -3,5 +3,5 @@ from .views import booking_request, booking_list
 
 urlpatterns = [
     path('<int:id_place>/', booking_request, name='booking_id'),
-    path('booking_list/', booking_list, name='booking_list')
+    path('booking_list/', booking_list, name='booking_list1')
 ]
